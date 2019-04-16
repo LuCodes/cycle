c1ec86aaf49d4815354604096b18ba83c9dde732
Lucas Blamont <lblamont@mac.com>
2019-04-16 11:20:58 +0200

    MAJ MAR16

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Wild from './Wild';
+
 
 class App extends Component {
+
+  state={
+    fields: {},
+  };
+
+  onSubmit = (fields) => {
+    this.setState({ fields });
+    console.log('App comp got:', fields);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Wild />
       </div>
     );
   }
---
+++ src/Wild.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Wild extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+    
+      componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+
+      componentDidUpdate() {
+        console.log('Un update a eu lieu');
+    }
+    
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+      
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+      
+    render() { 
+        return (  
+            <div>
+            <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+            
+        );
+    }
+}
+ 
+export default Wild;
